84e79895168488e8414a03be73fa0eebe1b2dafb
Aasif Saneeth <saneeth@titumlk.onmicrosoft.com>
2025-02-19 17:50:23 +0530

    server running issue fixed

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-import express from "express"
-import cors from "cors"
-import  session from "express-session"
-import dotenv from "dotenv"
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import dotenv from "dotenv";
 
+// Load environment variables from a .env file into process.env
+dotenv.config();
 
 const app = express();
 
 app.use(session({
-    secret:process.env.SESS_SECRET,
-    resave:false,
-    saveUninitialized:true,
-    cookie:{
-        secure:"auto"
+    secret: process.env.SESS_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        secure: "auto"
     }
+}));
 
-}
-))
-
-app.cors(cors({
-    credentials:true,
-    origin:"htttp://localhost:3000"
-}))
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:3000"
+}));
 
 app.use(express.json());
 
-app.listen(process.env.APP_PORT, ()=> {
-    console.log("server up and running...")
-})
+app.listen(process.env.APP_PORT, () => {
+    console.log("Server up and running...");
+});
