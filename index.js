a8d50d4863ea7c1bcf7945ca707b5db12e6f80af
Aasif Saneeth <saneeth@titumlk.onmicrosoft.com>
2025-02-19 17:33:05 +0530

    Data base Connection

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 import express from "express"
 import cors from "cors"
 import  session from "express-session"
+import dotenv from "dotenv"
+
 
 const app = express();
+
+app.use(session({
+    secret:process.env.SESS_SECRET,
+    resave:false,
+    saveUninitialized:true,
+    cookie:{
+        secure:"auto"
+    }
+
+}
+))
+
+app.cors(cors({
+    credentials:true,
+    origin:"htttp://localhost:3000"
+}))
+
+app.use(express.json());
 
 app.listen(process.env.APP_PORT, ()=> {
     console.log("server up and running...")
